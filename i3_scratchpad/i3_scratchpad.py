880defb4f1caab958bbdded200e889c1c4dfd6cd
Yoni Weill <joweill@icloud.com>
2018-11-13 03:52:07 +0200

    *

--- i3_scratchpad/i3_scratchpad.py
+++ i3_scratchpad/i3_scratchpad.py
@@ -76,17 +76,28 @@ def move_to_scratchpad(container):
     container.command('move scratchpad')
 
 
-def toggle_last_container(tree, process_workspace_containers):
+def toggle_last_container(tree, process_workspace_containers, dock):
     focused = tree.find_focused()
     if is_scratchpad_container(focused):
         move_to_scratchpad(focused)
         return
+    focused_workspace = focused.workspace()
     if process_workspace_containers:
-        for container in get_scratchpad_containers(focused.workspace()):
-            if is_floating(container):
-                move_to_scratchpad(container)
-                return
+        sp_containers = get_scratchpad_containers(focused_workspace)
+        if sp_containers:
+            move_to_scratchpad(sp_containers[0])
+            return
+        # for container in get_scratchpad_containers(focused_workspace):
+        #     if is_floating(container):
+        #         move_to_scratchpad(container)
+        #         return
     containers = tree.scratchpad().leaves()
     if not containers:
         return
-    containers[-1].command('move workspace current, focus')
+    if dock:
+        focused_workspace.command('split horizontal')
+        containers[-1].command(
+            'move workspace current, focus, floating disable, '
+            'resize set 50 ppt, move left, move left, move left')
+    else:
+        containers[-1].command('move workspace current, focus')
